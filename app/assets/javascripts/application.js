427e306b0c62ec0d8cbce019b3584df50fa74663
lukasx <llukasxx@gmail.com>
2016-02-22 08:09:47 -0800

    Initializing react

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
+//= require turbolinks//= require react
+//= require react_ujs
+//= require components
+
 //= require_tree .
