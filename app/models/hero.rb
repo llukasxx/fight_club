24a65929ddd9b895e57728d9488840e3e96eecbb
lukasx <llukasxx@gmail.com>
2016-02-22 04:59:11 -0800

    Add hero validation specs

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,2 +1,5 @@
 class Hero < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :description, presence: true
 end
--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:description) }
 end
