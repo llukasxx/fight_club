bedc8fe5e7e24e1632e40a05f320104a037b5c75
lukasx <llukasxx@gmail.com>
2016-02-25 17:29:43 -0800

    Add percentage calculations to fight

--- app/assets/javascripts/components/fights/fight_predictions.es6.jsx
+++ app/assets/javascripts/components/fights/fight_predictions.es6.jsx
@@ -1,6 +1,7 @@
 class FightPredictions extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {hostChance: "50%", guestChance: "50%"};
   }
   calculatePowerSkills(skills) {
     let wind = 0;
@@ -26,29 +27,45 @@ class FightPredictions extends React.Component {
     return {wind: wind, water:water, earth:earth, fire: fire};
   }
   calculateDifference(hostSkills, guestSkills) {
-    let hostAdvantage = {
-      wind: hostSkills.wind - guestSkills.wind,
-      water: hostSkills.water - guestSkills.water,
-      earth: hostSkills.earth - guestSkills.earth,
-      fire: hostSkills.fire - guestSkills.fire,
+    let advantage = {
+      wind_earth: hostSkills.wind - guestSkills.earth,
+      water_fire: hostSkills.water - guestSkills.fire,
+      earth_water: hostSkills.earth - guestSkills.water,
+      fire_wind: hostSkills.fire - guestSkills.wind,
     };
-    return hostAdvantage;
+    return advantage;
+  }
+  calculatePercentage(advantage) {
+    //55% base chance for host winning
+    let base = 55;
+    //each advantage-disadvantage point +/- 3%
+    let skillModifier = (advantage.wind_earth + advantage.water_fire 
+                        + advantage.earth_water + advantage.fire_wind) * 3;
+    let hostChance = base + skillModifier;
+
+    return hostChance;
   }
   componentWillReceiveProps(nextProps) {
     if(nextProps.host && nextProps.guest) {
       let hostSkills = this.calculatePowerSkills(nextProps.host.skills);
       let guestSkills = this.calculatePowerSkills(nextProps.guest.skills);
-      this.calculateDifference(hostSkills, guestSkills);
-      console.log(this.calculateDifference(hostSkills, guestSkills));
+      let skillAdvantage = this.calculateDifference(hostSkills, guestSkills);
+      let hostChance = this.calculatePercentage(skillAdvantage);
+      let guestChance = 100 - hostChance;
+      this.setState({hostChance: String(hostChance) + "%", guestChance: String(guestChance) + "%"})
+    } else {
+      this.setState({hostChance: "50%", guestChance: "50%"});
     }
   }
   render() {
     return (
       <div className="center-block text-center">
         <div className="progress">
-          <div className="progress-bar progress-bar-info"  style={{width:"50%"}}>
+          <div className="progress-bar progress-bar-info"  style={{width: this.state.hostChance}}>
+            {this.state.hostChance}
           </div>
-          <div className="progress-bar progress-bar-warning" style={{width:"50%"}}>
+          <div className="progress-bar progress-bar-warning" style={{width: this.state.guestChance}}>
+            {this.state.guestChance}
           </div>
         </div>
       </div>
