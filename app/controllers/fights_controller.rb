ae6ae708de7bba30de4f3c754bc378aaeba9d7e1
lukasx <llukasxx@gmail.com>
2016-02-27 09:34:17 -0800

    Add back-end weather modifiers

--- app/controllers/fights_controller.rb
+++ app/controllers/fights_controller.rb
@@ -22,12 +22,23 @@ class FightsController < ApplicationController
   private
 
     def calculate_chances(host, guest)
+      # Skill mod
       total_power_diff = calc_power_diff(host, guest)
+      # Skill element mod
       host_element_mod_advantage = calc_element_modifiers_advantage(host, guest)
       guest_element_mod_advantage = calc_element_modifiers_advantage(guest, host)
+      # Weather mod
+      weather = ["windy", "rainy", "dry", "hot"]
+      chosen_weather = weather.sample
+      host_weather_mod = calc_weather_mod(host, chosen_weather) - calc_weather_mod(guest, chosen_weather)
+      # Exp lvl mod
       host_level_mod = calc_level_mod(host, guest)
-      host_chance = 53 + (total_power_diff * 3) + (host_level_mod * 3) +
+      # Total chances
+      host_chance = 53 + (total_power_diff * 3) + (host_level_mod * 3) + host_weather_mod +
                          (host_element_mod_advantage - guest_element_mod_advantage)
+      guest_chance = 100 - host_chance
+
+      chances = {host_chance: host_chance, guest_chance: guest_chance, weather: chosen_weather}
     end
 
     def calc_power_diff(host, guest)
@@ -68,6 +79,19 @@ class FightsController < ApplicationController
         end
       end
       advantage
+    end
+
+    def calc_weather_mod(hero, weather)
+      case weather
+      when "windy"
+        hero.wind_power * 2
+      when "rainy"
+        hero.water_power * 2
+      when "dry"
+        hero.earth_power * 2
+      when "hot"
+        hero.fire_power * 2
+      end
     end
 
     def calc_level_mod(host, guest)
